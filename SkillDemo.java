ccdab5a11981e0e24daa306d2adfbfe7e39e9d69
Ruojiang-Zhang <ruz026@ucsd.edu>
2022-05-01 23:00:16 -0700

    a failed test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo{
+    public static int subtraction(int a, int b){
+        return a-b;
+    }
+}
---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void subtraction(){
+        assertEquals(3, SkillDemo.subtraction(5,3));
+    }
+}
